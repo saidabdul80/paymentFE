30d07f929a90657bcd1ae1af21422c7059670157
saidabdul80 <saidabdulsalam5@gmail.com>
2024-06-01 18:05:43 +0100

    fix: updates

--- src/components/dataTable/DataTable.vue
+++ src/components/dataTable/DataTable.vue
@@ -1,6 +1,6 @@
 <template>
   <v-card flat>
-    <v-card-title class="d-flex align-center pe-2"> 
+    <v-card-title class="d-flex align-center pe-2">
       USERS
       <v-spacer></v-spacer>
 
@@ -10,7 +10,6 @@
         label="Search"
         prepend-inner-icon="mdi-magnify"
         variant="solo-filled"
-        flat
         hide-details
         single-line
       ></v-text-field>
@@ -18,29 +17,30 @@
 
     <v-divider></v-divider>
     <v-data-table show-select v-model:search="search" :items="items">
-        <template v-slot:header.status>
-            <div class="text-end">status</div>
-        </template>
+      <template v-slot:header.status>
+        <div class="text-end">status</div>
+      </template>
 
-        <template v-slot:item.PHOTO="{ item }">
-            <v-card class="my-2 tw-h-9 tw-w-9" elevation="2" style="border-radius: 50%">
-                <v-img
-                    :src="`https://cdn.vuetifyjs.com/docs/images/graphics/gpus/${item.PHOTO}`"
-                ></v-img>
-            </v-card>
-        </template>
+      <template v-slot:item.PHOTO="{ item }">
+        <v-card class="my-2 tw-h-9 tw-w-9" elevation="2" style="border-radius: 50%">
+          <v-img
+            :src="`https://cdn.vuetifyjs.com/docs/images/graphics/gpus/${item.PHOTO}`"
+          ></v-img>
+        </v-card>
+      </template>
 
-        <template v-slot:item.status="{ item }">
-            <div class="text-end">
-            <v-chip
-                :class="getClasses(item.status)"
-                class="tw-w-[50px]"
-                :text="item.status"
-                size="x-small"
-                label
-            ></v-chip>
-            </div>
-        </template>
+      <template v-slot:item.status="{ item }">
+        <div class="text-end">
+          <span> </span>
+          <v-chip
+            :color="getColor(item.status)"
+            class="tw-w-[50px]"
+            :text="item.status"
+            size="x-small"
+            label
+          ></v-chip>
+        </div>
+      </template>
     </v-data-table>
   </v-card>
 </template>
@@ -53,17 +53,15 @@ export default {
       required: true
     }
   },
-  methods:{
-    getClasses(status){
-      let classname = ''
-      if(status?.toLowerCase() === 'active'){
-        classname = `tw-text-[${this.$constants.primary}] tw-bg-[${this.$constants.primary}]/${this.$constants.transparent_level}`
-      }else if(status?.toLowerCase() == 'pending'){
-        classname = `tw-text-[${this.$constants.warning}] tw-bg-[${this.$constants.warning}]/${this.$constants.transparent_level}` 
-      }else{
-        classname = `tw-text-[${this.$constants.danger}] tw-bg-[${this.$constants.danger}]/${this.$constants.transparent_level}`;
+  methods: {
+    getColor(status) {
+      if (status?.toLowerCase() === 'active') {
+        return `${this.$constants.primary}`
+      } else if (status?.toLowerCase() === 'pending') {
+        return `${this.$constants.warning}`
+      } else {
+        return `${this.$constants.danger}`
       }
-      return classname
     }
   },
   data() {
@@ -73,3 +71,6 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+</style>
