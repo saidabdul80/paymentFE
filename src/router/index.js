48774dc4d3091a1e273b798c2fd9f4ad204e6dc7
unknown <sabdulsalam@ubtconsults.se>
2024-06-13 07:03:38 +0100

    fix: update

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,13 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import AdminRoutes from '@/admin/admin-router'
+let routes = []
+routes = routes.concat(AdminRoutes)
 
 const router = createRouter({
   history: createWebHistory(),
   linkActiveClass: 'active',
-  AdminRoutes
+  routes
 })
 
 router.beforeEach((to, from, next) => {  
