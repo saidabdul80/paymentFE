84bed0a4d495aa61254cf418e0446e4d9edbcafd
unknown <sabdulsalam@ubtconsults.se>
2024-06-13 06:28:04 +0100

    fix: updates

---
+++ dist/assets/Index-BO-jyIz_.js
@@ -0,0 +1,1 @@
+import{_ as c,c as e,o as t}from"./index-V994NcY4.js";const o={},s={class:"tw-px-5"};function _(n,a){return t(),e("div",s)}const d=c(o,[["render",_],["__scopeId","data-v-72681c60"]]);export{d as default};
