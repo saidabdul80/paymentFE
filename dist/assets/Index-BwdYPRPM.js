e0e644040e2ae87308ca3d5031f35c0ceb08ca9e
unknown <sabdulsalam@ubtconsults.se>
2024-06-13 06:06:20 +0100

    fix: updates

--- dist/assets/Index-BwdYPRPM.js
+++
@@ -1,1 +0,0 @@
-import{_ as c,c as e,o as t}from"./index-abnNJ1tZ.js";const o={},s={class:"tw-px-5"};function _(n,a){return t(),e("div",s)}const d=c(o,[["render",_],["__scopeId","data-v-72681c60"]]);export{d as default};
---
+++ dist/assets/Index-CXbVMypx.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index-B8Gg0l6r.js";const n={};function r(c,t){return null}const o=e(n,[["render",r]]);export{o as default};
